fb9650d7006de52a19882f3af24aca849f25f4d4
Patriott07 <pwangtampn@gmail.com>
2025-05-09 05:13:05 +0700

    changes error register and login

--- controllers/auth.cotroller.js
+++ controllers/auth.cotroller.js
@@ -5,10 +5,10 @@ const salt = 10;
 
 export const login = async (req, res) => {
     try {
-        const user = await User.findOne({ email: req.email });
+        const user = await User.findOne({ email: req.body.email });
         if (!user) throw new Error("Cannot find any account, please try again");
 
-        const isMatch = bycrypt.compareSync(req.password, user.password);
+        const isMatch = bycrypt.compareSync(req.body.password, user.password);
         if (!isMatch) throw new Error("Authentication failed, dint match any");
 
         res.status(200).json({ message: "Your Account succesfully registed", user })
@@ -18,32 +18,33 @@ export const login = async (req, res) => {
     }
 }
 
-export const register = async (req, res) => {
-    try {
-        const property = {
-            name: req.name,
-            email: req.email,
-            password: bycrypt.hash(req.password, salt),
-            level: 0,
-            profile: "",
-            exp: 0,
-            trophy: 0,
-            winrate : 100,
-            match : 0
+    export const register = async (req, res) => {
+        console.log({req})
+        try {
+            const property = {
+                name: req.body.name,
+                email: req.body.email,
+                password: bycrypt.hash(req.password.body, salt),
+                level: 0,
+                profile: "",
+                exp: 0,
+                trophy: 0,
+                winrate : 100,
+                match : 0
+            }
+
+            const user = await User.create(property);
+
+            if (user) {
+                return res.status(200).json({ message: "Berhasil loginn..", user })
+            }
+
+        } catch (error) {
+            console.log({ err })
+            return res.status(502).json({ message: "Error : ".error })
         }
 
-        const user = await User.create(property);
-
-        if (user) {
-            return res.status(200).json({ message: "Berhasil loginn..", user })
-        }
-
-    } catch (error) {
-        console.log({ err })
-        return res.status(502).json({ message: "Error : ".error })
     }
-
-}
 
 export const me = async (req, res) => {
     try {
