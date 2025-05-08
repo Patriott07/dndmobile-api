ce2cb6087fb7163a1de3b81a2d6d15301446af4d
Patriott07 <pwangtampn@gmail.com>
2025-05-09 06:36:15 +0700

    change

--- controllers/auth.cotroller.js
+++ controllers/auth.cotroller.js
@@ -11,7 +11,7 @@ export const login = async (req, res) => {
         const isMatch = bycrypt.compareSync(req.body.password, user.password);
         if (!isMatch) throw new Error("Authentication failed, dint match any");
 
-        res.status(200).json({ message: "Your Account succesfully registed", user })
+        res.status(200).json({ message: "Login Succesfully", user })
     } catch (error) {
         console.log({ error })
         return res.status(502).json({ message: `Error : ${error}` });
@@ -37,7 +37,7 @@ export const register = async (req, res) => {
         const user = await User.create(property);
 
         if (user) {
-            return res.status(200).json({ message: "Berhasil loginn..", user })
+            return res.status(200).json({  title : "Your Account Regitsered", message: "Thanks for trusting us, now all of your progress on gamewill be safe on your account, gooduluck warrior!", user })
         }
 
     } catch (error) {
