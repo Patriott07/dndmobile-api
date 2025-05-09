8cd6348a280b2bc9b71e4871c0e8ddc85048a263
Patriott07 <pwangtampn@gmail.com>
2025-05-09 12:35:54 +0700

    change

--- controllers/card.controller.js
+++ controllers/card.controller.js
@@ -1,8 +1,9 @@
 import { timeStamp } from "console";
 import { Card } from "../models/Cards.model.js";
 
-export const get = async(req, res) => {
-    const cards = await Card.find({}).sort({createdAt : -1});
-    
-    return res.status(200).json({cards});
+export const get = async (req, res) => {
+    const cards = await Card.find({}).sort({ createdAt: -1 });
+    const length = cards.length;
+
+    return res.status(200).json({ cards, length });
 }
