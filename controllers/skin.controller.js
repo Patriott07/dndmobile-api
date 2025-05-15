83ded6cb90591f1dc630b3ba154d9a81381c0abe
Patriott07 <pwangtampn@gmail.com>
2025-05-15 12:21:26 +0700

    change

--- controllers/skin.controller.js
+++ controllers/skin.controller.js
@@ -2,7 +2,7 @@ import { Skin } from "../models/Skin.model.js";
 
 export const getAllSkin = async (req, res) => {
     try {
-        const skins = Skin.find({}).sort({ createdAt: -1 });
+        const skins = await Skin.find({}).sort({ createdAt: -1 });
         return res.status(200).json({ skins });
     } catch (error) {
         console.log({ error })
